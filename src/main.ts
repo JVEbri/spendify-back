de66a7d464b7235c4299b74d2bf4c0b5e745c589
JVEbri <ebrigomezjv@gmail.com>
2025-02-01 13:27:11 +0100

    vercel json

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { writeFileSync } from 'fs';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
