c85d979c23cb3014f74cf860e3fab2050b21034f
JVEbri <ebrigomezjv@gmail.com>
2025-05-27 00:00:23 +0200

    uncomment

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -3,5 +3,5 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest(); // Obtiene la request desde el contexto
   console.log('Request user', request.user);
-  return request.user; // Retorna el usuario extraído del token JWT
+  return request.user;
 });
